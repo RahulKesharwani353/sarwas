b2772e0afc8971237fc8e93692c69c1b16960f7b
Rahulkesharwani353 <rahul.kesharwani@cm.com>
2023-05-19 17:44:10 +0530

    setting changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Developer_Zone\SARWAS\sarwas\venv
+set VIRTUAL_ENV=C:\Mystuffs\sarwas\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
